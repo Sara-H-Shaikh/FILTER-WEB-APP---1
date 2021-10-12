853cc2752e3e525f0bfa59dce95986bafa567519
Sara-H-Shaikh <87994090+Sara-H-Shaikh@users.noreply.github.com>
2021-10-12 16:15:25 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,21 @@
+function preload()
+{
+    clown_nose = loadImage('https://i.postimg.cc/PxFvYgkv/l1.png');
+}
+
+function setup()
+{
+    canvas = createCanvas(300,300);
+    canvas.center();
+}
+
+
+function draw()
+{
+  
+}
+
+function take_snapshot()
+{
+    save("my_picture.png");
+}
